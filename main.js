f4c7835550b880c8affa48c369564ce1f6c6b8ed
Noam <noamru22@gmail.com>
2020-07-23 16:27:57 +0200

    finishing touches

--- main.js
+++ main.js
@@ -16,176 +16,289 @@ let currentFrameCount;
 let timer = 30;
 
 function preload() {
-    console.log("preload");
-    game.preload();
-    this.mariposa = loadImage("assets/mariposa.png");
-    this.textX = 0;
-    this.textY = 80;
+  console.log("preload");
+  game.preload();
+  this.mariposa = loadImage("assets/mariposa.png");
+  this.textX = 0;
+  this.textY = 80;
 }
 
 function setup() {
-    console.log("setup");
-    // game has not started, so mode should be 0
-    mode = 0;
-    canvas = createCanvas(1100, 700);
-    canvas.parent("canvas");
-    textSize(25);
-    textFont("Georgia");
-    textStyle(NORMAL);
-    // setInterval(timeIsUp, 800);
-    game.setup();
+  console.log("setup");
+  // game has not started, so mode should be 0
+  canvas = createCanvas(1050, 650);
+  // mode = 0;
+  // canvas.parent("canvas");
+  // textSize(22);
+  // textFont("Georgia");
+  // textStyle(NORMAL);
+  // game.setup();
+  resetSketch();
+  loop();
 }
 
 function draw() {
-    // load startscreen
-    if (mode == 0) {
-        game.background.draw();
-    };
+  // load startscreen
+  if (mode == 0) {
+    game.background.draw();
     fill(color(500, 150));
-    // image(this.mariposa, 100, 100);
 
-    let instructions = "try to catch as many light orbs as possible in the given time. use your voice or any other sound to make the player move. catching a light orb will increase your playing-time.";
-    text(instructions, 30, height - 300, 444, 300, 50)
-    text("press enter to start, and enjoy!", 50, height - 100, 444, 200, 50)
+    let instructions =
+      "Catch as many light orbs as you can within the given time. The butterfly responds to frequency: clip your fingers or use a high voice to move it up and use a low voice to bring it down. Catching a light orb will increase your play-time. Make sure you are in a quite environment.";
+    rect(width / 4, 50, height - 80, 350, 50);
+    push();
+    fill(color(255, 202));
+    stroke("#222222");
+    strokeWeight(4.5);
+    text(instructions, 300, 100, 510, 300);
+    text("Press enter to start, and enjoy!", 400, 300, 510, 400);
+    pop();
+  }
+  // if enter button is pushed
+  if (mode == 1) {
+    game.draw();
 
-    // if enter button is pushed
-    if (mode == 1) {
-        game.draw();
+    // start timer
+    timeIsUp();
+    if (timer >= 60) {
+      let newTime = timer - 60;
+      if (newTime < 10) {
+        newTime = "0" + newTime;
+      }
+      text("1:" + newTime, width - 100, 60);
+    } else if (timer <= 60 && timer >= 10) {
+      text("0:" + timer, width - 100, 60);
+    } else {
+      text("0:0" + timer, width - 100, 60);
+    }
+    // when timer is 0 game over
+    if (timer == 0) {
+      game.background.assignBackgroundLevel(game.level);
+      fill(color(500, 150));
+      textFont("Georgia");
+      let gameOver = "GAME OVER!";
+      rect(width / 4, 50, height - 80, 350, 50);
+      push();
+      fill(color(255, 202));
+      stroke("#222222");
+      strokeWeight(4.5);
+      text(gameOver, 460, 150, 510, 300);
+      text(
+        `You reached level ${game.level} and your score is  ${game.player.score}`,
+        300,
+        200,
+        510,
+        400
+      );
+      pop();
+      // stop game
+      noLoop();
+      // make sure player can play again
+      // mode = 0;
+    }
 
-        // start timer
-        timeIsUp();
-        if (timer >= 60) {
-            let newTime = timer - 60
-            if (newTime < 10) {
-                newTime = "0" + newTime;
-            }
-            text("1:" + newTime, width - 100, 60);
-        } else if (timer <= 60 && timer >= 10) {
-            text("0:" + timer, width - 100, 60);
-        } else {
-            console.log("TEN NINE..")
-            text("0:0" + timer, width - 100, 60);
-        }
-        // when timer is 0 game over
-        if (timer == 0) {
-            text('GAME OVER', width / 3, height / 2 + 100);
-            // go to gameover screen, show score and level
-            game.background.assignBackgroundLevel(game.level);
-            text(`you reached level ${game.level} and your score is  ${game.player.score}`, width / 4, height / 2);
-            noLoop();
-        }
+    if (game.level == 1) {
+      game.background.assignBackgroundLevel(game.level);
+      // set timing for steppingstones
+      if (frameCount % 100 === 0) {
+        why = height / 5;
 
-        if (game.level == 1) {
-            game.background.assignBackgroundLevel(game.level);
-            // set timing for steppingstones
-            if (frameCount % 100 === 0) {
-                why = height / 2;
+        game.steppingstones.push(new SteppingStone(why));
+      } else if (frameCount % 120 === 0) {
+        why = height - 180;
+        game.steppingstones.push(new SteppingStone(why));
+      }
+      // change level
+      if (game.player.score == 100) game.level = 2;
+    } else if (game.level == 2) {
+      game.background.assignBackgroundLevel(game.level);
+      // set timing for steppingstones
+      if (frameCount % 150 === 0) {
+        // create new stone with different height
+        why = height / 4;
+        game.steppingstones.push(new SteppingStone(why));
+      } else if (frameCount % 60 === 0) {
+        why = height - 100;
+        game.steppingstones.push(new SteppingStone(why));
+      }
+      // change level
+      if (game.player.score == 200) {
+        game.level = 3;
+      }
+    } else if (game.level == 3) {
+      game.background.assignBackgroundLevel(game.level);
+      // set timing for steppingstones
+      if (frameCount % 30 === 0) {
+        // create new stone with different height
+        why = height - 100;
+        game.steppingstones.push(new SteppingStone(why));
+      }
+      // change level
+      if (game.player.score == 300) {
+        currentFrameCount = frameCount;
+        game.level = 4;
+      }
+    } else if (game.level == 4) {
+      // upload background pic
+      game.background.assignBackgroundLevel(game.level);
+      // set timing for steppingstones
+      if (
+        frameCount > currentFrameCount + 100 &&
+        frameCount % 10 === 0 &&
+        frameCount < currentFrameCount + 500
+      ) {
+        why = 200;
+        game.steppingstones.push(new SteppingStone(why));
+      } else if (
+        frameCount % 100 ===
+        0
+        // frameCount < currentFrameCount + 2000
+      ) {
+        why = height - 200;
+        game.steppingstones.push(new SteppingStone(why));
+      } else if (frameCount % 60 === 0) {
+        why = height / 4;
+        game.steppingstones.push(new SteppingStone(why));
+      }
+      // change level
+      if (game.player.score == 1000) {
+        currentFrameCount = frameCount;
+        game.level = 5;
+      }
 
-                game.steppingstones.push(new SteppingStone(why));
+      // finishing game screen
+      if (game.player.score > 1000 || game.level == 5) {
+        // go to win screen, show score and level
+        mode = 3;
+        // game.background.assignBackgroundLevel(1);
 
-            } else if (frameCount % 120 === 0) {
-                why = height - 280;
-                game.steppingstones.push(new SteppingStone(why));
-            }
-            // change level
-            if (game.player.score == 100) {
-                game.level = 2;
-            }
-        } else if (game.level == 2) {
-            game.background.assignBackgroundLevel(game.level);
-            // set timing for steppingstones
-            if ((frameCount % 120 === 0) && (frameCount % 150 === 0)) {
-                // create new stone with different height
-                why = height / 2;
-                game.steppingstones.push(new SteppingStone(why));
-            } else if (frameCount % 60 === 0) {
-                why = height - 280;
-                game.steppingstones.push(new SteppingStone(why));
-            }
-            // change level
-            if (game.player.score == 300) {
-                game.level = 3
-            }
-        } else if (game.level == 3) {
-            game.background.assignBackgroundLevel(game.level);
-            // set timing for steppingstones
-            if (frameCount % 30 === 0) {
-                // create new stone with different height
-                why = height / 2;
-                game.steppingstones.push(new SteppingStone(why));
-            }
-            // change level
-            if (game.player.score == 500) {
-                currentFrameCount = frameCount;
-                game.level = 4
-            }
-        } else if (game.level == 4) {
-            // activate frequency measurer
-            game.player.voice = 1;
-            // upload background pic
-            game.background.assignBackgroundLevel(game.level);
-            push()
-            textFont("Georgia");
-            text("use your voice", 0, this.textY, 600, 200);
-            pop()
-
-            // set timing for steppingstones
-            if ((frameCount > currentFrameCount + 100) && (frameCount % 10 === 0) && (frameCount < currentFrameCount + 500)) {
-                why = 100;
-                game.steppingstones.push(new SteppingStone(why));
-            } else if ((frameCount % 100 === 0) && (frameCount % 30 === 0) && (frameCount < currentFrameCount + 2000)) {
-                why = height - 200;
-                game.steppingstones.push(new SteppingStone(why));
-            } else if ((frameCount > currentFrameCount + 100) && (frameCount % 50 === 0) && (frameCount < currentFrameCount + 1000)) {
-                why = height - 150;
-                game.steppingstones.push(new SteppingStone(why))
-            } else if ((frameCount % 66 === 0) && (frameCount < currentFrameCount + 1000)) {
-                why = 300;
-                game.steppingstones.push(new SteppingStone(why))
-
-            }
-        };
-
-        // CHECK FOR COLISSION , REMOVE & ADD STONE, ADJUST SCORE
+        fill(color(500, 150));
+        rect(width / 4, 50, height - 80, 350, 50);
+        push();
+        fill(color(255, 202));
+        stroke("#222222");
+        strokeWeight(4.5);
+        textFont("Georgia");
+        text("C O N G R A T U L A T I O N S !", 350, 100, 510, 400);
+        text(
+          `You reached level ${game.level}, your score is ${game.player.score} and you even have ${timer} seconds left `,
+          310,
+          180,
+          510,
+          400
+        );
+        text("press enter to start again", 380, 320, 510, 400);
+        console.log(mode);
+        pop();
+        // remove last steppingstones
         game.steppingstones.forEach((steppingstone, index) => {
-            // if player touches steppingstone
-            if (steppingstone.isCollision(steppingstone, game.player)) {
-                // remove steppingstone when touched
-                game.steppingstones.splice(index, 1);
-                // increase score
-                game.player.score += 10;
-
-                // increase timer
-                timer += 1;
-            };
-            steppingstone.draw();
-            // if steppingstone leaves the canvas
-            if (steppingstone.x < -steppingstone.width) {
-                // decrease score 
-                game.player.score -= 10;
-                // and remove from array
-                game.steppingstones.splice(index, 1);
-            }
+          game.steppingstones.splice(index, game.steppingstones.length);
+          // noLoop();
         });
-    };
-};
+      }
+    }
+    // CHECK FOR COLISSION , REMOVE & ADD STONE, ADJUST SCORE
+    game.steppingstones.forEach((steppingstone, index) => {
+      // if player touches steppingstone
+      if (steppingstone.isCollision(steppingstone, game.player)) {
+        // remove steppingstone when touched
+        game.steppingstones.splice(index, 1);
+        // increase score
+        game.player.score += 10;
 
+        // increase timer
+        timer += 1;
+      }
+      steppingstone.draw();
+      // if steppingstone leaves the canvas
+      if (steppingstone.x < -steppingstone.width) {
+        // decrease score
+        game.player.score -= 10;
+        // and remove from array
+        game.steppingstones.splice(index, 1);
+
+        // show gameover screen when score is negative
+        if (game.player.score < 0) {
+          mode = 3;
+          game.background.assignBackgroundLevel(game.level);
+          fill(color(500, 150));
+          rect(width / 4, 50, height - 80, 350, 50);
+          push();
+          fill(color(255, 202));
+          stroke("#222222");
+          strokeWeight(4.5);
+          textFont("Georgia");
+          text("game over..", 470, 100, 510, 400);
+          text(
+            `you reached level ${game.level} and your score is`,
+            310,
+            180,
+            510,
+            400
+          );
+          text(`${game.player.score}`, 510, 220, 510, 400);
+          text("press enter to start again", 380, 320, 510, 400);
+          pop();
+          // remove last steppingstones
+          game.steppingstones.forEach((steppingstone, index) => {
+            game.steppingstones.splice(index, game.steppingstones.length);
+          });
+          // // stop game
+          // if (mode === 3) {
+          //   // noLoop();
+          // }
+        }
+      }
+    });
+  }
+}
 
 function keyPressed() {
-    if (keyCode === ENTER) {
-        mode = 1;
-        level = 1;
+  // restart game if game-over
+  if (
+    (keyCode === ENTER && game.player.score < 0) ||
+    (keyCode === ENTER && timer == 0) ||
+    (keyCode === ENTER && game.player.score > 1000) ||
+    (keyCode === ENTER && game.level == 5)
+  ) {
+    // reset values
+    mode = 0;
+    game.level = 1;
+    game.player.score = 0;
+    time = 30;
+    resetSketch();
+    console.log("mode after keypress", mode);
+  } else if (keyCode === ENTER) {
+    // loop();
+    mode = 1;
+    level = 1;
+    game.player.score = 0;
+    console.log("keypressed");
+    // game.setup();
+    // loop();
+    // start audiocontext
+    getAudioContext().resume();
+  }
+}
 
-        // start audiocontext
-        getAudioContext().resume();
-    }
+function resetSketch() {
+  mode = 0;
+  timer = 30;
+  // level = 1;
+  canvas.parent("canvas");
+  game.background.assignBackgroundLevel(1);
+  textSize(22);
+  textFont("Georgia");
+  textStyle(NORMAL);
+  game.setup();
+  loop();
 }
 
 function timeIsUp() {
-    if (frameCount % 60 == 0 && timer > 0) {
-        timer--;
-        console.log(timer);
-    }
+  if (frameCount % 60 == 0 && timer > 0) {
+    timer--;
+    console.log(timer);
+  }
 }
 
 const game = new Game();
--- player.js
+++ player.js
@@ -1,104 +1,102 @@
 class Player {
-    constructor() {
-        this.x = 0;
-        this.score = 0;
-        this.easing = 0.12;
-        this.voice = 0;
-    };
+  constructor() {
+    this.x = 0;
+    this.score = 0;
+    this.easing = 0.12;
+    this.voice = 0;
+  }
 
-    preload() {
-        console.log("player preload");
-        butterfly = loadImage("assets/butterfly1.png")
-        butterfly2 = loadImage("assets/butterfly2.png")
-        butterfly3 = loadImage("assets/butterfly3.png")
-    };
+  preload() {
+    console.log("player preload");
+    butterfly = loadImage("assets/butterfly1.png");
+    butterfly2 = loadImage("assets/butterfly2.png");
+    butterfly3 = loadImage("assets/butterfly3.png");
+  }
 
-    setup() {
-        console.log("player setup")
-        this.width = 80;
-        this.height = 80;
-        this.y = 650;
-        this.x = width / 4;
+  setup() {
+    console.log("player setup");
+    this.width = 80;
+    this.height = 80;
+    this.y = 650;
+    this.x = width / 4;
 
-        // initialize microphone input
-        mic = new p5.AudioIn();
-        mic.start();
-        fft = new p5.FFT();
-        fft.setInput(mic);
-    };
+    // initialize microphone input
+    mic = new p5.AudioIn();
+    mic.start();
+    fft = new p5.FFT();
+    fft.setInput(mic);
+  }
 
-    draw() {
-        // adjust score and level
-        textSize(28);
-        textFont('Helvetica');
-        text("SCORE: " + this.score, 40, 50);
-        fill(255);
-        text("LEVEL " + game.level, 40, 80);
-        if (game.level == "YOU ARE DONE!") {
-            text("SCORE: " + this.score, 40, 50);
-            fill(255);
-            text("LEVEL " + game.level, 40, 80);
-        }
+  draw() {
+    // adjust score and level
+    textSize(28);
+    textFont("Helvetica");
+    text("SCORE: " + this.score, 40, 50);
+    fill(255);
+    text("LEVEL " + game.level, 40, 80);
+    if (game.level == "YOU ARE DONE!") {
+      text("SCORE: " + this.score, 40, 50);
+      fill(255);
+      text("LEVEL " + game.level, 40, 80);
+    }
 
-        if (this.voice == 0) {
-            //use scaled volume as input for y position
-            let volume = mic.getLevel();
-            let audioHeight = map(volume * 4, 0, 1, height, 0);
+    if (this.voice == 0) {
+      // clear();
+      volume = mic.getLevel();
+      // store frequency
+      spectrum = fft.analyze();
 
-            // add easing to the player's position to slow down
-            const target = height - (audioHeight / 1.5);
-            this.y = this.y + (target - this.y) * this.easing;
+      let neededLowFreq = fft.getEnergy("bass");
+      let neededHighFreq = fft.getEnergy("highMid");
 
-            // make sure butterfly won't leave canvas
-            if (this.y > height) {
-                this.y = height - 100;
-            }
+      this.y = 300;
 
-            // change butterfly images to make him fly
-            if (frameCount % 5 == 0) {
-                // draw player using volume as y position
-                image(butterfly, this.x, this.y, this.width, this.height);
-            } else {
-                //draw player using volume as y position
-                image(butterfly2, this.x, this.y, this.width, this.height);
-            };
+      if (neededLowFreq > neededHighFreq) {
+        this.y = this.y + neededLowFreq * 4;
+      } else {
+        this.y = this.y - neededHighFreq * 3;
+      }
 
-            //  enable frequency input when arriving to last level
-        } else {
-            // clear();
-            volume = mic.getLevel();
-            // store frequency
-            spectrum = fft.analyze();
+      // make sure butterfly won't leave canvas
+      if (this.y > height) {
+        this.y = height - 100;
+      }
 
-            let neededLowFreq = fft.getEnergy("bass");
-            let neededHighFreq = fft.getEnergy("highMid");
+      if (this.y < 0) {
+        this.y = 100;
+      }
 
-            this.y = 300;
+      // change butterfly images to make him fly
+      if (frameCount % 5 == 0) {
+        // draw player using volume as y position
+        image(butterfly, this.x, this.y, this.width, this.height);
+      } else {
+        //draw player using volume as y position
+        image(butterfly2, this.x, this.y, this.width, this.height);
+      }
+      //  enable frequency input when arriving to last level
+    } else {
+      //use scaled volume as input for y position
+      let volume = mic.getLevel();
+      let audioHeight = map(volume * 4, 0, 1, height, 0);
 
-            if (neededLowFreq > neededHighFreq) {
-                this.y = this.y + (neededLowFreq * 4);
-            } else {
-                this.y = this.y - (neededHighFreq * 3);
-            }
+      // add easing to the player's position to slow down
+      const target = height - audioHeight / 1.5;
+      this.y = this.y + (target - this.y) * this.easing;
 
-            // make sure butterfly won't leave canvas
-            if (this.y > height) {
-                this.y = height - 100;
-            }
+      // make sure butterfly won't leave canvas
+      if (this.y > height) {
+        this.y = height - 100;
+      }
 
-            if (this.y < 0) {
-                this.y = 100;
-            }
-
-            // change butterfly images to make him fly
-            if (frameCount % 5 == 0) {
-                // draw player using volume as y position
-                image(butterfly, this.x, this.y, this.width, this.height);
-            } else {
-                //draw player using volume as y position
-                image(butterfly2, this.x, this.y, this.width, this.height);
-            };
-        };
-    };
-
-};
+      // change butterfly images to make him fly
+      if (frameCount % 5 == 0) {
+        // draw player using volume as y position
+        image(butterfly, this.x, this.y, this.width, this.height);
+      } else {
+        //draw player using volume as y position
+        image(butterfly2, this.x, this.y, this.width, this.height);
+      }
+    }
+  }
+}
